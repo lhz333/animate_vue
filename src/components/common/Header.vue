0e9bf3b039a086e84e2292a09d085f7b4aa1698b
lihuizhen <huizhen.li@charleskeith.com>
2020-08-27 11:58:59 +0800

    feat: 布局

---
+++ src/components/common/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data () {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import wow from 'wowjs';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import "@/assets/css/reset.css";
 import 'animate.css';
+
+Vue.use(ElementUI);
 
 Vue.prototype.$wow = wow;
 
